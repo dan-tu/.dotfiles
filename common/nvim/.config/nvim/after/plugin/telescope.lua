168c8d15412d43ca74996cec0c830c56ca3e4168
Dan Tu <dantu22@live.com>
2023-01-03 13:19:58 -0800

    Add git branches and cleanup treesitter

--- common/nvim/.config/nvim/after/plugin/telescope.lua
+++ common/nvim/.config/nvim/after/plugin/telescope.lua
@@ -4,6 +4,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {noremap=true})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {noremap=true})
 vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, {noremap=true})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {noremap=true})
+vim.keymap.set('n', '<leader>gc', builtin.git_branches, {noremap=true})
 
 
 require("telescope").setup({
--- common/nvim/.config/nvim/after/plugin/treesitter.lua
+++ common/nvim/.config/nvim/after/plugin/treesitter.lua
@@ -12,6 +12,6 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
-    enabled = true,
+    additional_vim_regex_highlighting = false,
   },
 }
